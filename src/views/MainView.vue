8a3cc639cf706d92ba82e9f80ee4a461015727e8
LSeolAh <snxwxh@gmail.com>
2023-05-01 00:30:32 +0900

    vue setting

---
+++ src/views/MainView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="background">
+      <img src="@/assets/image/cloudy.gif" />
+    </div>
+    <div class="left_box">
+      <div class="left_box-search">
+        <input type="text" placeholder="Seach" />
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+    <!-- <div class="day">
+      <h2>2023 May 01 Monday</h2>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
