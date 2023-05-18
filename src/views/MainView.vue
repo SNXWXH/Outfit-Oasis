a25eb0f4e7f87f286c8dc9cf43abe160a032ba54
LSeolAh <snxwxh@gmail.com>
2023-05-18 11:35:55 +0900

    Add slide

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -9,6 +9,49 @@
         <input type="text" placeholder="Search" />
         <i class="fa-solid fa-magnifying-glass"></i>
       </div>
+      <div class="detail">
+        <v-window
+          class="detail__slide"
+          v-model="onboarding"
+          show-arrows="hover"
+        >
+          <v-window-item :key="`card-${n}`">
+            <v-card elevation="2" class="detail__v-card">
+              <h1>Today outfit</h1>
+              <div class="detail__v-card-outfit">
+                <h2>• 방수 가능한 가벼운 자켓 또는 우비</h2>
+                <h2>• 방수 가능한 가벼운 자켓 또는 우비</h2>
+                <h2>• 방수 가능한 가벼운 자켓 또는 우비</h2>
+                <h2>• 방수 가능한 가벼운 자켓 또는 우비</h2>
+                <h2>• 방수 가능한 가벼운 자켓 또는 우비</h2>
+              </div>
+            </v-card>
+          </v-window-item>
+          <v-window-item :key="`card-${n}`">
+            <v-card elevation="2" class="detail__v-card">
+              <h1>Weather Detail</h1>
+              <div class="detail__v-card-weather">
+                <div class="detail__v-card-temp">
+                  <h2>Max/Min Temp</h2>
+                  <h2>3°C / 18°C</h2>
+                </div>
+                <div class="detail__v-card-humidity">
+                  <h2>Humidity</h2>
+                  <h2>60%</h2>
+                </div>
+                <div class="detail__v-card-wind">
+                  <h2>Wind</h2>
+                  <h2>734km/h</h2>
+                </div>
+                <div class="detail__v-card-cloud">
+                  <h2>Cloudy</h2>
+                  <h2>80%</h2>
+                </div>
+              </div>
+            </v-card>
+          </v-window-item>
+        </v-window>
+      </div>
       <div class="right-box">
         <div class="right-box__day">
           <h2>2023 May 01 Monday</h2>
@@ -49,7 +92,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    length: 2,
+    onboarding: 0,
+  }),
+  methods: {
+    next() {
+      this.onboarding =
+        this.onboarding + 1 > this.length ? 1 : this.onboarding + 1;
+    },
+    prev() {
+      this.onboarding =
+        this.onboarding - 1 <= 0 ? this.length : this.onboarding - 1;
+    },
+  },
+};
 </script>
 
 <style></style>
