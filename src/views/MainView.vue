29973c22ea8812ebe3aeaf86e141261e013f2d30
LSeolAh <snxwxh@gmail.com>
2023-05-18 11:37:20 +0900

    Setting font

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -110,4 +110,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+* {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
