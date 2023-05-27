3f351396d8287c2185e548c7532fb210ceeb988c
LSeolAh <snxwxh@gmail.com>
2023-05-28 00:44:03 +0900

    Connect Weather API

--- back-end/src/api/index.js
+++ back-end/src/api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import location from "./routes/geolocation";
+import weather from "./routes/weather";
 
 export default () => {
   const router = express.Router();
 
   location(router);
+  weather(router);
   // console.log("router");
   // console.log(router.get("/", (req, res) => res.send("hi")));
 
--- back-end/src/config/index.js
+++ back-end/src/config/index.js
@@ -9,5 +9,6 @@ if (envFound.error) {
 export default {
   naver_access_key: process.env.NAVER_ACCESS_KEY,
   naver_secret_key: process.env.NAVER_SECRET_KEY,
+  weather_key: process.env.WEATHER_KEY,
   port: process.env.PORT,
 };
